8ef07f5e2f8b4de4bf261e9df6f0d75b24c63f6b
scchang <a0928136004@gmail.com>
2023-12-03 11:05:02 +0800

    modified

--- test_main.py
+++ test_main.py
@@ -10,57 +10,63 @@ from pico_i2c_lcd import I2cLcd
 
 def connect_to_internet(ssid, password):
     # Pass in string arguments for ssid and password
-
     # Just making our internet connection
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     wlan.connect(ssid, password)
-
     # Wait for connect or fail
     max_wait = 10
     while max_wait > 0:
+        print(wlan.status(), max_wait)
         if wlan.status() < 0 or wlan.status() >= 3:
             break
         max_wait -= 1
         print('waiting for connection...')
-        time.sleep(1)
+        utime.sleep(1)
     # Handle connection error
     if wlan.status() != 3:
         print(wlan.status())
-        raise RuntimeError('network connection failed')
+        # raise RuntimeError('network connection failed')
+        print('network connection failed')
     else:
         print('connected')
         print(wlan.status())
         status = wlan.ifconfig()
+    return wlan.status()
 
 # ssid = "linkou_203_1F"
-ssid = "linkou203-4F"
-password = "56665666"
+# ssid = "linkou203-4F"
+ssid = "scchang_iphone"
+# password = "56665666"
+password = "0928136004"
 blynk_token = "BBITOQPAWXszFKZzMp6YKlQ-88vRqAl0"
 
 sensor = DHT11(Pin(22, Pin.OUT, Pin.PULL_DOWN))
+
 I2C_ADDR     = 39
 I2C_NUM_ROWS = 2
 I2C_NUM_COLS = 16
-
 i2c = I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=400000)
 lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
 
-# led=machine.Pin(2, machine.Pin.OUT)
-led=machine.Pin("LED", machine.Pin.OUT)
+led=machine.Pin(15, machine.Pin.OUT)
+build_in_led=machine.Pin("LED", machine.Pin.OUT)
 led.off()
+build_in_led.off()
 
-connect_to_internet(ssid, password)
+if connect_to_internet(ssid, password) == 3:
+    build_in_led.on()
+else:
+    build_in_led.off()
+
 BLYNK = Blynk(blynk_token)
-
 # Register virtual pin handler
 @BLYNK.on("V2") #virtual pin V0
 def v0_write_handler(value): #read the value
     if int(value[0]) == 1:
         led.value(1) #turn the led on        
     else:
-        led.value(0)    #turn the led off
-
+        led.value(0) #turn the led off
 while True:
     try:
         sensor.measure()
