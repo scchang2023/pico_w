d470dfc2852c0562492aced74e408ea9e3be748b
scchang <a0928136004@gmail.com>
2025-05-16 16:34:58 +0800

    modified

---
+++ ctrl_fans/main.py
@@ -0,0 +1,109 @@
+from machine import I2C,Pin
+import utime
+import network
+import sys
+sys.path.append("/lib")
+from blynklib import Blynk
+from dht import DHT11
+from lcd_api import LcdApi
+from pico_i2c_lcd import I2cLcd
+
+# ssid = "linkou_203_1F"
+# ssid = "linkou203-4F"
+ssid = "scchang_iphone"
+# password = "56665666"
+password = "0928136004"
+blynk_token = "BBITOQPAWXszFKZzMp6YKlQ-88vRqAl0"
+
+def wifi_connect(ssid, password):
+    # Pass in string arguments for ssid and password
+    # Just making our internet connection
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+    # Wait for connect or fail
+    max_wait = 10
+    while max_wait > 0:
+        print(wlan.status(), max_wait)
+        if wlan.status() < 0 or wlan.status() >= 3:
+            break
+        max_wait -= 1
+        print('wait for connecting.')
+        utime.sleep(1)
+    print(wlan.status())
+    if wlan.status() != 3:
+        # raise RuntimeError('network connection failed')
+        print('failed to connect wifi.')
+    else:
+        print('connected')
+        status = wlan.ifconfig()
+    
+    return wlan.status()
+    
+def dht11_init():
+    sensor = DHT11(Pin(22, Pin.OUT, Pin.PULL_DOWN))
+    return sensor
+
+def dht11_get_temp_humi(sensor):
+    sensor.measure()
+    temp = sensor.temperature()
+    humi = sensor.humidity()
+    return temp, humi
+
+def lcd_init():
+    I2C_ADDR     = 39
+    I2C_NUM_ROWS = 2
+    I2C_NUM_COLS = 16
+    i2c = I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=400000)
+    lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
+    return lcd
+
+def lcd_display_temp_humi(lcd, temp, humi):
+    str1 = f"temp:{temp} *C"
+    lcd.move_to(0,0)
+    lcd.putstr(str1)
+    lcd.move_to(0,1)
+    str1 = f"hum:{humi} %"
+    lcd.putstr(str1)    
+
+def led_init():
+    led=machine.Pin(15, machine.Pin.OUT)
+    build_in_led=machine.Pin("LED", machine.Pin.OUT)
+    led.off()
+    build_in_led.off()
+    return led, build_in_led
+
+def main():
+    sensor = dht11_init()
+    lcd = lcd_init()
+    led, build_in_led = led_init()
+    wlan_status = wifi_connect(ssid, password)
+
+    if wlan_status == 3:
+        BLYNK = Blynk(blynk_token)
+        build_in_led.on()
+        # Register virtual pin handler
+        @BLYNK.on("V2") #virtual pin V0
+        def v0_write_handler(value): #read the value
+            if int(value[0]) == 1:
+                led.value(1) #turn the led on        
+            else:
+                led.value(0) #turn the led off
+    else:
+        build_in_led.off()
+
+    while True:
+        try:
+            temp, humi = dht11_get_temp_humi(sensor)
+            print(temp, humi)
+            lcd_display_temp_humi(lcd, temp, humi)
+            #if wlan_status == 3:
+            #    BLYNK.virtual_write(0, temp)
+            #    BLYNK.virtual_write(1, humi)
+            #    BLYNK.run()
+        except:
+            print("The checksum of dht11 was invalid")
+        utime.sleep(1)
+
+if __name__ == "__main__":
+    main()
